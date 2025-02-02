f10b8f0c02fe4cb49b3a636870c91b4e9c4327bb
avirzin <arthur@virzin.me>
2025-02-02 10:39:53 -0300

    Fixed some thing in Tokenized Money

--- backend/contracts/TokenizedMoney.sol
+++ backend/contracts/TokenizedMoney.sol
@@ -3,17 +3,6 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// TokenizedGold.sol - ERC-20 token representing tokenized gold
-contract TokenizedGold is ERC20, Ownable {
-    constructor() ERC20("Tokenized Gold", "tGOLD") {
-        _mint(msg.sender, 1000000 * 10 ** decimals()); // Initial supply
-    }
-
-    function mint(address to, uint256 amount) external onlyOwner {
-        _mint(to, amount);
-    }
-}
-
 // TokenizedMoney.sol - ERC-20 token representing the loan currency (CBDC-like)
 contract TokenizedMoney is ERC20, Ownable {
     constructor() ERC20("Tokenized Money", "tMONEY") {
